070659cab968bfa7649304c85b4052cbd8f47510
Joachim MASSON <joachim.masson.17@gmail.com>
2025-12-15 10:05:27 +0100

    create route /

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,6 +6,8 @@ import "dotenv/config";
 // is not required when starting the application
 import "../database/checkConnection";
 
+import type { RequestHandler } from "express";
+
 // Import the Express application from ./app
 import app from "./app";
 
@@ -20,3 +22,9 @@ app
   .on("error", (err: Error) => {
     console.error("Error:", err.message);
   });
+
+const welcome: RequestHandler = (req, res) => {
+  res.send("Welcome to wild series!");
+};
+
+app.get("/", welcome);
